dccac9426c67a60f23c820fe900374ce7dc45ac0
Eric Oestrich <eric@oestrich.org>
2020-04-16 20:52:12 -0400

    Add health endpoint

--- lib/web/controllers/page_controller.ex
+++ lib/web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule Web.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def health(conn, _params) do
+    send_resp(conn, 200, "OK\n")
+  end
 end
--- lib/web/router.ex
+++ lib/web/router.ex
@@ -42,6 +42,8 @@ defmodule Web.Router do
     post("/register/reset/verify", RegistrationResetController, :update)
 
     get("/users/confirm", ConfirmationController, :confirm)
+
+    get("/_health", PageController, :health)
   end
 
   scope "/", Web do
