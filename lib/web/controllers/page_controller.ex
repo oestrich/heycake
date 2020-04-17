93a137c6fa726fc6ec131c7a30b9a4378fdf39cf
Eric Oestrich <eric@oestrich.org>
2020-04-17 09:20:15 -0400

    Assign an opengraph url

--- lib/web/controllers/page_controller.ex
+++ lib/web/controllers/page_controller.ex
@@ -1,10 +1,13 @@
 defmodule Web.PageController do
   use Web, :controller
 
+  alias Web.Router.Helpers, as: Routes
+
   def index(conn, _params) do
     conn
     |> assign(:open_graph_title, "heycake")
     |> assign(:open_graph_description, "Let your team eat 🍰")
+    |> assign(:open_graph_url, Routes.page_url(conn, :index))
     |> render("index.html")
   end
 
